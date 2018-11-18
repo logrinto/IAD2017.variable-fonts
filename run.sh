cf0e6a67cc3fbd148dc6e340863ea171b1204cba
signalwerk <sh@signalwerk.ch>
2018-11-19 00:33:12 +0300

    ADD: run script

---
+++ run.sh
@@ -0,0 +1,29 @@
+
+fontmake -o variable -m ./src/stephanie/stephanie.designspace
+
+fontmake -o variable -m ./src/tieu/tieu.designspace
+
+fontmake -o variable -m ./src/nadia/nadia.designspace
+fontmake -o variable -m ./src/natasha/natasha.designspace
+fontmake -o variable -m ./src/wolfgang/wolfgang.designspace
+
+
+fontmake -o variable -m ./src/matthias/matthiaskoch.designspace
+fontmake -o variable -m ./src/lars/lars.designspace
+
+
+fontmake -o variable -m ./src/marc/marc.designspace
+
+
+fontmake -o variable -m ./src/marius/marius.designspace
+fontmake -o variable -m ./src/nicole/nicole.designspace
+
+
+fontmake -o variable -m ./src/nils/nils.designspace
+fontmake -o variable -m ./src/pascale/pascale.designspace
+fontmake -o variable -m ./src/severin/severin.designspace
+
+# rm -rf ./src/nils/ufo/Nils-bold.ufo
+# rm -rf ./src/nils/ufo/Nils-regular.ufo
+# ./vfb2ufo ./src/nils/Nils_bold.vfb ./src/nils/ufo/Nils-bold.ufo
+# ./vfb2ufo ./src/nils/Nils_regular.vfb ./src/nils/ufo/Nils-regular.ufo
